bc1f9d11a5fbb9c138f5809f37e0987a5a204ce1
daniel <lagarto.sparrow@gmail.com>
2018-08-10 16:37:40 -0400

    mi primer commit

---
+++ assets/js/script.js
@@ -0,0 +1,197 @@
+$(function(){
+
+        var body = $('body'),
+                stage = $('#stage'),
+                back = $('a.back');
+
+        /* Step 1 */
+
+        $('#step1 .encrypt').click(function(){
+                body.attr('class', 'encrypt');
+
+                // Go to step 2
+                step(2);
+        });
+
+        $('#step1 .decrypt').click(function(){
+                body.attr('class', 'decrypt');
+                step(2);
+        });
+
+        /* Step 2 */
+
+        $('#step2 .button').click(function(){
+                // Trigger the file browser dialog
+                $(this).parent().find('input').click();
+        });
+
+        // Set up events for the file inputs
+
+        var file = null;
+
+        $('#step2').on('change', '#encrypt-input', function(e){
+
+                // Has a file been selected?
+
+                if(e.target.files.length!=1){
+                        alert('Please select a file to encrypt!');
+                        return false;
+                }
+
+                file = e.target.files[0];
+
+                if(file.size > 1024*1024){
+                        alert('Please choose files smaller than 1mb, otherwise you may crash your browser. nThis is a known issue. See the tutorial.');
+                        return;
+                }
+
+                step(3);
+        });
+
+        $('#step2').on('change', '#decrypt-input', function(e){
+
+                if(e.target.files.length!=1){
+                        alert('Please select a file to decrypt!');
+                        return false;
+                }
+
+                file = e.target.files[0];
+                step(3);
+        });
+
+        /* Step 3 */
+
+        $('a.button.process').click(function(){
+
+                var input = $(this).parent().find('input[type=password]'),
+                        a = $('#step4 a.download'),
+                        password = input.val();
+
+                input.val('');
+
+                if(password.length<5){
+                        alert('Please choose a longer password!');
+                        return;
+                }
+
+                // The HTML5 FileReader object will allow us to read the 
+                // contents of the      selected file.
+
+                var reader = new FileReader();
+
+                if(body.hasClass('encrypt')){
+
+                        // Encrypt the file!
+
+                        reader.onload = function(e){
+
+                                // Use the CryptoJS library and the AES cypher to encrypt the 
+                                // contents of the file, held in e.target.result, with the password
+								var key = CryptoJS.enc.Utf8.parse('12EstaClave34es56dificil489ssswf');
+								var iv = CryptoJS.enc.Utf8.parse('Devjoker7737hAES');
+								/*
+								var data = [];
+								for (var i = 0; i < key.length; i++){  
+									data.push(key.charCodeAt(i));
+								}
+								var data2 = [];
+								for (var i = 0; i < iv.length; i++){  
+									data2.push(iv.charCodeAt(i));
+								}*/
+								//var texto = CryptoJS.enc.Utf8.parse(e.target.result);
+								var encrypted = CryptoJS.AES.encrypt(e.target.result, key, {
+								blockSize: 128/8,
+								keySize: 256/8,
+								iv: iv,
+								mode: CryptoJS.mode.CBC,
+								padding: CryptoJS.pad.Pkcs7
+								});
+
+                                //var encrypted = CryptoJS.AES.encrypt(e.target.result, password, {iv: iv});
+
+                                // The download attribute will cause the contents of the href
+                                // attribute to be downloaded when clicked. The download attribute
+                                // also holds the name of the file that is offered for download.
+
+                                a.attr('href', 'data:application/octet-stream,' + encrypted);
+                                a.attr('download', file.name);
+
+                                step(4);
+                        };
+
+                        // This will encode the contents of the file into a data-uri.
+                        // It will trigger the onload handler above, with the result
+
+                        reader.readAsDataURL(file);
+                }
+                else {
+
+                        // Decrypt it!
+
+                        reader.onload = function(e){
+							
+								var key = CryptoJS.enc.Utf8.parse('12EstaClave34es56dificil489ssswf');
+								var iv = CryptoJS.enc.Utf8.parse('Devjoker7737hAES');
+							
+								var decrypted = CryptoJS.AES.decrypt(e.target.result, key, {
+								blockSize: 128/8,
+								keySize: 256/8,
+								iv: iv,
+								mode: CryptoJS.mode.CBC,
+								padding: CryptoJS.pad.Pkcs7
+								})
+                                                                                .toString(CryptoJS.enc.Latin1);
+																				
+								/*
+                                var decrypted = CryptoJS.AES.decrypt(e.target.result, password)
+                                                                                .toString(CryptoJS.enc.Latin1);
+								*/
+                                if(!/^data:/.test(decrypted)){
+                                        alert("Invalid pass phrase or file! Please try again.");
+                                        return false;
+                                }
+
+                                a.attr('href', decrypted);
+                                a.attr('download', file.name.replace('.encrypted',''));
+
+                                step(4);
+                        };
+
+                        reader.readAsText(file);
+                }
+        });
+
+        /* The back button */
+
+        back.click(function(){
+
+                // Reinitialize the hidden file inputs,
+                // so that they don't hold the selection 
+                // from last time
+
+                $('#step2 input[type=file]').replaceWith(function(){
+                        return $(this).clone();
+                });
+
+                step(1);
+        });
+
+        // Helper function that moves the viewport to the correct step div
+
+        function step(i){
+
+                if(i == 1){
+                        back.fadeOut();
+                }
+                else{
+                        back.fadeIn();
+                }
+
+                // Move the #stage div. Changing the top property will trigger
+                // a css transition on the element. i-1 because we want the
+                // steps to start from 1:
+
+                stage.css('top',(-(i-1)*100)+'%');
+        }
+
+});
